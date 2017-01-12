e41a05e30e01107fd6a16b517b7106422c1fd956
Andres Gonzalez <gonche1124@gmail.com>
2017-01-12 00:18:32 +0000

    Added template and some embedded graphs

--- app.py
+++ app.py
@@ -4,11 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+     return render_template('index.html')
 
+
+@app.route('/exploration_steps', methods=['GET', 'POST'])
+def exploration_steps():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return 'Post was clicked'
+
+    # show the form, it wasn't submitted
+    return render_template('index2.html')
+
+
+##Run Main Application
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.debug = True
+    app.run(port=33507)
